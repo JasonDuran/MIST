f14e881213274421056df6d022356541740822d9
JDuran <Jason.Duran@sv.cmu.edu>
2013-09-14 13:50:36 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MIST::Application.config.secret_token = 'ad4455151e2b288c42a935a19fe5c189dcb1b6f9329afc74c0be4609a36f123d1691d52dcc6d917e47604e2c0735cefab795152894d9bcb03eef32659c4cc5c3'
